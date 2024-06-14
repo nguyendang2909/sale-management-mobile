6095d21b76385854186feccff4752e4549a65972
nguyendang2909 <nguyendang2909@gmail.com>
2024-06-14 05:22:52 +0000

    feat: sale statistics

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,8 +1,35 @@
-import { View } from '@gluestack-ui/themed';
+import { Pressable, View } from '@gluestack-ui/themed';
 import { FC, ReactNode } from 'react';
 import { ViewProps } from 'src/types';
 
-export const Card: FC<ViewProps & { children?: ReactNode }> = ({ children, ...viewProps }) => {
+export const Card: FC<ViewProps & { children?: ReactNode; onPress?: () => void }> = ({
+  children,
+  onPress,
+  ...viewProps
+}) => {
+  if (onPress) {
+    return (
+      <Pressable onPress={onPress}>
+        {/* @ts-ignore */}
+        {({ pressed }) => (
+          <View
+            bgColor={pressed ? '$coolGray200' : '$white'}
+            borderRadius={8}
+            shadowOffset={{
+              width: 0,
+              height: 1,
+            }}
+            shadowOpacity={0.25}
+            shadowRadius={3}
+            elevation={3}
+            {...viewProps}
+          >
+            {children}
+          </View>
+        )}
+      </Pressable>
+    );
+  }
   return (
     <View
       bgColor="$white"
--- src/screens/management/views/cards-feature/card-feature.tsx
+++ src/screens/management/views/cards-feature/card-feature.tsx
@@ -1,4 +1,4 @@
-import { Pressable, Text, View } from '@gluestack-ui/themed';
+import { Text, View } from '@gluestack-ui/themed';
 import { FC, useCallback } from 'react';
 import { BaseIcon, Card } from 'src/components';
 import { navigate } from 'src/navigations';
@@ -11,20 +11,17 @@ export const CardFeature: FC<{ data: NavigationCard }> = ({ data }) => {
   }, [data.screen]);
 
   return (
-    <Pressable w="$1/3" px={8} py={8} onPress={handlePress}>
-      {/* @ts-ignore */}
-      {({ pressed }) => (
-        <Card flex={1} p={16} bgColor={pressed ? '$coolGray200' : '$white'}>
-          <View justifyContent="center" alignItems="center">
-            <BaseIcon size={32} icon={data.icon} />
-          </View>
-          <View mt={4}>
-            <Text textAlign="center" size="xs" numberOfLines={1}>
-              {data.title}
-            </Text>
-          </View>
-        </Card>
-      )}
-    </Pressable>
+    <View w="$1/3" px={8} py={8}>
+      <Card flex={1} p={16} onPress={handlePress}>
+        <View justifyContent="center" alignItems="center">
+          <BaseIcon size={32} icon={data.icon} />
+        </View>
+        <View mt={4}>
+          <Text textAlign="center" size="xs" numberOfLines={1}>
+            {data.title}
+          </Text>
+        </View>
+      </Card>
+    </View>
   );
 };
