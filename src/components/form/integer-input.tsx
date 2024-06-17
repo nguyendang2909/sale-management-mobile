771d963b8e39531f97baf7506cc0a5cb71dfef36
nguyendang2909 <nguyendang2909@gmail.com>
2024-06-17 18:12:28 +0000

    feat: verify specification when create product

--- src/components/form/integer-input.tsx
+++ src/components/form/integer-input.tsx
@@ -14,7 +14,7 @@ import {
   VStack,
 } from '@gluestack-ui/themed';
 import _ from 'lodash';
-import React, { useCallback } from 'react';
+import React, { forwardRef, useCallback } from 'react';
 import { InputModeOptions } from 'react-native';
 
 import { MaterialIcons } from '../icon';
@@ -22,7 +22,7 @@ import { MaterialIcons } from '../icon';
 type FCProps = {
   label: string;
   onChange: (e: number | null) => void;
-  value?: number;
+  value?: number | null;
   error?: string;
   maxLength?: number;
   placeholder?: string;
@@ -33,70 +33,77 @@ type FCProps = {
   focusable?: boolean;
 };
 
-export const IntegerInput: React.FC<FCProps> = ({
-  label,
-  onChange,
-  value,
-  error,
-  maxLength,
-  placeholder,
-  testID,
-  isRequired,
-  inputMode,
-  onBlur,
-  focusable,
-}) => {
-  const handleClear = useCallback(() => {
-    onChange(null);
-  }, [onChange]);
-
-  const handleChange = useCallback(
-    (e: string) => {
-      if (e) {
-        const eNumber = +e.replaceAll('.', '').replace(',', '');
-        if (!isNaN(eNumber) && _.isNumber(eNumber)) {
-          if (eNumber < Number.MAX_SAFE_INTEGER) {
-            onChange(eNumber);
-          }
-          return;
-        }
-      }
+export const IntegerInput = forwardRef(
+  (
+    {
+      label,
+      onChange,
+      value,
+      error,
+      maxLength,
+      placeholder,
+      testID,
+      isRequired,
+      inputMode,
+      onBlur,
+      focusable,
+    }: FCProps,
+    ref,
+  ) => {
+    const handleClear = useCallback(() => {
       onChange(null);
-    },
-    [onChange],
-  );
+    }, [onChange]);
 
-  return (
-    <FormControl {...(isRequired ? { isRequired } : {})} isInvalid={!!error}>
-      <VStack>
-        <FormControlLabel>
-          <FormControlLabelText>{label}</FormControlLabelText>
-        </FormControlLabel>
-        <Input testID={testID} variant="underlined">
-          <InputField
-            focusable={focusable}
-            inputMode={inputMode}
-            value={value ? value.toString() : ''}
-            onChangeText={handleChange}
-            placeholder={placeholder}
-            maxLength={maxLength}
-            onBlur={onBlur}
-          ></InputField>
-          {!!value && (
-            <InputSlot onPress={handleClear}>
-              <InputIcon as={CloseCircleIcon}></InputIcon>
-            </InputSlot>
-          )}
-        </Input>
-        <View pb={12}>
-          <FormControlError position="absolute">
-            {/* 
-            // @ts-ignore */}
-            <FormControlErrorIcon as={MaterialIcons} name="error-outline"></FormControlErrorIcon>
-            <FormControlErrorText>{error}</FormControlErrorText>
-          </FormControlError>
-        </View>
-      </VStack>
-    </FormControl>
-  );
-};
+    const handleChange = useCallback(
+      (e: string) => {
+        if (e) {
+          const eNumber = +e.replaceAll('.', '').replace(',', '');
+          if (!isNaN(eNumber) && _.isNumber(eNumber)) {
+            if (eNumber < Number.MAX_SAFE_INTEGER) {
+              onChange(eNumber);
+            }
+            return;
+          }
+        }
+        onChange(null);
+      },
+      [onChange],
+    );
+
+    return (
+      <FormControl {...(isRequired ? { isRequired } : {})} isInvalid={!!error}>
+        <VStack>
+          <FormControlLabel>
+            <FormControlLabelText>{label}</FormControlLabelText>
+          </FormControlLabel>
+          <Input testID={testID} variant="underlined">
+            <InputField
+              focusable={focusable}
+              inputMode={inputMode}
+              value={value ? value.toString() : '0'}
+              onChangeText={handleChange}
+              placeholder={placeholder}
+              maxLength={maxLength}
+              onBlur={onBlur}
+              // @ts-ignore
+              ref={ref}
+            ></InputField>
+            {!!value && (
+              <InputSlot onPress={handleClear}>
+                <InputIcon as={CloseCircleIcon}></InputIcon>
+              </InputSlot>
+            )}
+          </Input>
+          <View pb={12}>
+            <FormControlError position="absolute">
+              {/* 
+              // @ts-ignore */}
+              <FormControlErrorIcon as={MaterialIcons} name="error-outline"></FormControlErrorIcon>
+              <FormControlErrorText>{error}</FormControlErrorText>
+            </FormControlError>
+          </View>
+        </VStack>
+      </FormControl>
+    );
+  },
+);
--- src/screens/product-create/views/form/product-stock.control.tsx
+++ src/screens/product-create/views/form/product-stock.control.tsx
@@ -1,12 +1,21 @@
 import { View } from '@gluestack-ui/themed';
-import { ComponentProps, FC } from 'react';
+import { ComponentProps, FC, useEffect, useRef } from 'react';
 import { Control, Controller } from 'react-hook-form';
+import { TextInput } from 'react-native';
 import { IntegerInput } from 'src/components/form/integer-input';
 import { FormParams } from 'src/types';
 
 export const ProductStockControl: FC<
   ComponentProps<typeof View> & { control: Control<FormParams.CreateProduct, any> }
 > = ({ control, ...viewProps }) => {
+  const textInputRef = useRef<TextInput>();
+
+  useEffect(() => {
+    if (textInputRef.current && textInputRef.current.focus) {
+      textInputRef.current.focus();
+    }
+  }, []);
+
   return (
     <>
       <Controller
@@ -16,9 +25,11 @@ export const ProductStockControl: FC<
           <View {...viewProps}>
             <IntegerInput
               label="Tồn kho"
-              value={field.value?.toString()}
+              value={field.value || 0}
               onChange={field.onChange}
               focusable={true}
+              ref={textInputRef}
+              inputMode="numeric"
             />
           </View>
         )}
